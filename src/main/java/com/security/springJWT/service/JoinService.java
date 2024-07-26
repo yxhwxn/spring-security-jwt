4f311a27c5f443384d119c440d9ed42f7943043c
yxhwxn <officeyh9819@gmail.com>
2024-07-26 23:52:56 +0900

    회원가입 로직 구현

---
+++ src/main/java/com/security/springJWT/service/JoinService.java
@@ -0,0 +1,39 @@
+package com.security.springJWT.service;
+
+import com.security.springJWT.controller.dto.JoinDTO;
+import com.security.springJWT.domain.UserEntity;
+import com.security.springJWT.domain.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public void joinProcess(JoinDTO joinDTO) {
+
+        String username = joinDTO.getUsername();
+        String password = joinDTO.getPassword();
+
+        Boolean isExist = userRepository.existsByUsername(username);
+
+        if(isExist) {
+            throw new IllegalArgumentException("이미 존재하는 유저입니다.");
+        }
+
+        UserEntity data = new UserEntity();
+
+        data.setUsername(username);
+        data.setPassword(bCryptPasswordEncoder.encode(password));
+        data.setRole("ROLE_ADMIN");
+
+        userRepository.save(data);
+    }
+}
