1a269a2079f63007dcfeb5eb1294246d285604e4
yxhwxn <officeyh9819@gmail.com>
2024-07-26 23:52:46 +0900

    회원가입 로직 구현

---
+++ src/main/java/com/security/springJWT/controller/JoinController.java
@@ -0,0 +1,26 @@
+package com.security.springJWT.controller;
+
+import com.security.springJWT.controller.dto.JoinDTO;
+import com.security.springJWT.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+}
